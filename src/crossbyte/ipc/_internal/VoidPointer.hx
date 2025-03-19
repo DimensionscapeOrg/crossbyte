f0d07eee7ae1339a24a1397501642b279ab51ebf
Chris Speciale <christopher.gaf@gmail.com>
2025-03-19 06:20:57 -0400

    Implement LocalConnection
    Provides a basic implementation for LocalConnection IPC for Windows

---
+++ src/crossbyte/ipc/_internal/VoidPointer.hx
@@ -0,0 +1,9 @@
+package crossbyte.ipc._internal;
+import cpp.Pointer;
+import cpp.Void as CppVoid;
+
+/**
+ * ...
+ * @author Christopher Speciale
+ */
+typedef VoidPointer = Pointer<CppVoid>;
---
+++ src/crossbyte/ipc/_internal/win/HANDLE.hx
@@ -0,0 +1,15 @@
+package crossbyte.ipc._internal.win;
+import crossbyte.ipc._internal.VoidPointer;
+
+/**
+ * ...
+ * @author Christopher Speciale
+ */
+
+abstract HANDLE(VoidPointer) to VoidPointer
+{	
+	@:from
+	static inline function fromPointer(ptr:VoidPointer):HANDLE{
+		return cast ptr;
+	}
+}
