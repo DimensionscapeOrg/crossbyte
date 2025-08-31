7bdb7661fdcf7558dddee26eda56461e9878822f
Chris Speciale <christopher.gaf@gmail.com>
2025-08-31 00:05:10 -0400

    Implement ServerApplication
    Extends Application and create a entrypoint tailored toward Server apps.

---
+++ src/crossbyte/core/ServerApplication.hx
@@ -0,0 +1,26 @@
+package crossbyte.core;
+import crossbyte.events.Event;
+import sys.thread.Thread;
+
+@:access(crossbyte.core.CrossByte)
+class ServerApplication extends Application{
+    private function new():Void{
+        super();
+    }
+
+    override function initialize() {        
+        if (Application.__application != null) {
+			throw "ServerApplication must only be instantiated once by extending it.";
+		}
+
+		// Ensure we're in the main thread
+		if (Thread.current() != Application.__mainThread) {
+			throw "ServerApplication must only be instantiated in the main thread!";
+		}
+
+		Application.__application = this;
+		__crossByte = new CrossByte(true, POLL);
+		__crossByte.addEventListener(Event.INIT, __onInit);
+		__crossByte.addEventListener(Event.EXIT, __onExit);
+    }
+}
