470c2333e51d54035c5233317b2d0bbf37a2e95e
Chris Speciale <christopher.gaf@gmail.com>
2025-09-01 07:20:28 -0400

    Remove ITimer

--- src/crossbyte/_internal/system/timer/ITimer.hx
+++
@@ -1,8 +0,0 @@
-package crossbyte._internal.system.timer;
-
-interface ITimer{
-    public var size(get, never):Int;
-	public var isEmpty(get, never):Bool;	
-	public var time(get, never):Float;
-	public final startTime:Float;
-}
