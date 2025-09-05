6adee24afe4683c9e1e5e5b21a4c280d25c8afb7
Chris Speciale <christopher.gaf@gmail.com>
2025-09-05 01:15:35 -0400

    Logger: add new separator function

--- src/crossbyte/utils/Logger.hx
+++ src/crossbyte/utils/Logger.hx
@@ -12,4 +12,8 @@ class Logger {
 	public static function error(message:String):Void {
 		Sys.println("[ERROR] " + message);
 	}
+
+	public static function separator():Void{
+		Sys.println("-------------------------------------");
+	}
 }
