5c9a0738a733026cb7ec09a48ebd5da87b21a56b
Chris Speciale <christopher.gaf@gmail.com>
2026-04-28 04:19:56 -0400

    Fix ByteArrayOutput written-length bookkeeping

--- tests/crossbyte/io/ByteArrayOutputTest.hx
+++ tests/crossbyte/io/ByteArrayOutputTest.hx
@@ -4,6 +4,31 @@ import haxe.io.Bytes;
 import utest.Assert;
 
 class ByteArrayOutputTest extends utest.Test {
+	public function testInitialCapacityDoesNotLeakUnusedBytesIntoOutput():Void {
+		var output = new ByteArrayOutput(8);
+		output.writeInt(0x12345678);
+
+		var bytes:Bytes = output;
+
+		Assert.equals(4, bytes.length);
+		var input:ByteArrayInput = ByteArray.fromBytes(bytes);
+		Assert.equals(0x12345678, input.readInt());
+		Assert.isTrue(input.eof());
+	}
+
+	public function testReserveDoesNotLeakUnusedBytesIntoOutput():Void {
+		var output = new ByteArrayOutput();
+		output.reserve(8);
+		output.writeInt(0x12345678);
+
+		var bytes:Bytes = output;
+
+		Assert.equals(4, bytes.length);
+		var input:ByteArrayInput = ByteArray.fromBytes(bytes);
+		Assert.equals(0x12345678, input.readInt());
+		Assert.isTrue(input.eof());
+	}
+
 	public function testToBytesFlushesUnflushedBytes():Void {
 		var output = new ByteArrayOutput();
 		output.reserve(6);
@@ -43,5 +68,16 @@ class ByteArrayOutputTest extends utest.Test {
 		Assert.equals(0x11, input.readByte());
 		Assert.equals(0x55667788, input.readInt());
 		Assert.isTrue(input.eof());
+	}
+
+	public function testValidateSizeUsesCurrentChunkCapacity():Void {
+		var output = new ByteArrayOutput(1);
+		output.writeByte(0x11);
+		output.reserve(4);
+
+		Assert.isTrue(output.validateSize(4));
+		Assert.isFalse(output.validateSize(5));
+		Assert.isTrue(output.validateSizeAt(2, 2));
+		Assert.isFalse(output.validateSizeAt(3, 2));
 	}
 }
