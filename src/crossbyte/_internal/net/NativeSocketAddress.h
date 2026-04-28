fe6578c73087fcaf3426b56a2da61eaa6858061c
Chris Speciale <christopher.gaf@gmail.com>
2026-04-28 02:21:32 -0400

    Avoid hxcpp include from native socket header

--- src/crossbyte/_internal/net/NativeSocketAddress.h
+++ src/crossbyte/_internal/net/NativeSocketAddress.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <hxcpp.h>
-
 Dynamic crossbyte_socket_accept(Dynamic socket);
 Array<int> crossbyte_socket_host_info(Dynamic socket);
 Array<int> crossbyte_socket_peer_info(Dynamic socket);
