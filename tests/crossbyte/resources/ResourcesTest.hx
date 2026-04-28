139762ec91bce52009b854bbef9a951b786c1d00
Chris Speciale <christopher.gaf@gmail.com>
2026-04-28 07:29:06 -0400

    Harden resource loading and add resource fixtures

--- tests/crossbyte/resources/ResourcesTest.hx
+++ tests/crossbyte/resources/ResourcesTest.hx
@@ -1,6 +1,7 @@
 package crossbyte.resources;
 
 import crossbyte.Resources;
+import StringTools;
 import utest.Assert;
 
 class ResourcesTest extends utest.Test {
@@ -12,5 +13,39 @@ class ResourcesTest extends utest.Test {
 	public function testResourceTreeExistsWithoutResourcesDirectory():Void {
 		Assert.notNull(Resources.tree);
 		Assert.notNull(Resources.resourcesDir);
+	}
+
+	public function testResourceTreeExposesCompileTimePaths():Void {
+		Assert.equals("testsuite/sample.txt", Resources.tree.testsuite.sample_txt);
+		Assert.equals("testsuite/sample.json", Resources.tree.testsuite.sample_json);
+		Assert.equals("testsuite/nested/child.txt", Resources.tree.testsuite.nested.child_txt);
+	}
+
+	public function testTextBytesJsonAndLinesLoadResourceContent():Void {
+		Assert.isTrue(Resources.exists("testsuite/sample.txt"));
+		Assert.equals("alpha\nbeta\ngamma\n", Resources.getText("testsuite/sample.txt"));
+		Assert.equals("alpha\nbeta\ngamma\n", Resources.getBytes("testsuite/sample.txt").toString());
+
+		var lines = Resources.getLines("testsuite/sample.txt");
+		Assert.same(["alpha", "beta", "gamma"], lines);
+
+		var json:crossbyte.TypedObject<{name:String, count:Int}> = Resources.getJSON("testsuite/sample.json");
+		Assert.equals("crossbyte", json.name);
+		Assert.equals(3, json.count);
+	}
+
+	public function testResourceListingAndAbsolutePathsStayRelativeToResourcesRoot():Void {
+		var direct = Resources.listResources("testsuite");
+		direct.sort(Reflect.compare);
+		Assert.same(["nested", "sample.json", "sample.txt"], direct);
+
+		var recursive = Resources.listResourcesRecursive("testsuite");
+		recursive.sort(Reflect.compare);
+		Assert.same(["testsuite/nested/child.txt", "testsuite/sample.json", "testsuite/sample.txt"], recursive);
+
+		var absolutePath = StringTools.replace(Resources.getAbsolutePath("testsuite/sample.txt"), "\\", "/");
+		Assert.notEquals(-1, absolutePath.indexOf("/resources/"));
+		Assert.isTrue(StringTools.endsWith(absolutePath, "/sample.txt"));
+		Assert.equals(Resources.getBytes("testsuite/sample.txt").length, Resources.resourceSize("testsuite/sample.txt"));
 	}
 }
