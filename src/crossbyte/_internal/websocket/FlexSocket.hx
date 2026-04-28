68bff019b315f0d4123e89773395118ce2581f93
Chris Speciale <christopher.gaf@gmail.com>
2026-04-27 22:22:11 -0400

    Reference nested types in FlexSocket typedefs
    Update typedefs in src/crossbyte/_internal/websocket/FlexSocket.hx to point to the nested types on crossbyte._internal.socket.FlexSocket (HostInfo and Sockets) instead of the top-level socket package. This fixes type resolution when those types are declared as inner types of FlexSocket.

--- src/crossbyte/_internal/websocket/FlexSocket.hx
+++ src/crossbyte/_internal/websocket/FlexSocket.hx
@@ -1,5 +1,5 @@
 package crossbyte._internal.websocket;
 
 typedef FlexSocket = crossbyte._internal.socket.FlexSocket;
-typedef HostInfo = crossbyte._internal.socket.HostInfo;
-typedef Sockets = crossbyte._internal.socket.Sockets;
+typedef HostInfo = crossbyte._internal.socket.FlexSocket.HostInfo;
+typedef Sockets = crossbyte._internal.socket.FlexSocket.Sockets;
